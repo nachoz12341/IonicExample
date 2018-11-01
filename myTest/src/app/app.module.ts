19b85b282fa488bbede0585ae976869fdacfc9bb
nachoz12341 <nachoz12341@gmail.com>
2018-10-31 20:55:36 -0500

    Added QR Scanner code
    needs work, issue with plugin

--- myTest/src/app/app.module.ts
+++ myTest/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AboutPage} from '../pages/about/about';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    QRScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- myTest/src/pages/about/about.ts
+++ myTest/src/pages/about/about.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+export class AboutPage
+{
+  constructor(private qrScanner: QRScanner)
+  {
+  }
 
+  scan()
+  {
+    console.log('Clicked');
+
+    this.qrScanner.prepare().then((status: QRScannerStatus) =>
+    {
+
+      console.log('Status authorized: '+status.authorized);
+
+      if(status.authorized)
+      {
+        let scansub = this.qrScanner.scan().subscribe((text: string) =>
+        {
+          this.qrScanner.hide();
+          scansub.unsubscribe();
+        });
+      }
+      else if (status.denied)
+      {
+      }
+      else
+      {
+      }
+    });
   }
 
 }
